75b31aa42fbdf6503f09db60ad021f491f64c836
Ayoub Rossi <ayoub.rossi@outlook.fr>
2023-11-13 16:34:20 +0100

    first commit

---
+++ src/main/java/com/ayrossi/demo/LiquibaseBugApplication.java
@@ -0,0 +1,13 @@
+package com.ayrossi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseBugApplication.class, args);
+	}
+
+}
